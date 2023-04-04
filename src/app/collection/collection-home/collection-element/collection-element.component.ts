c5df2a9ab324daed40fb058317305a2fdc3f1203
Jedrzej Niemiera <jedrzejniemiera@gmail.com>
2023-04-04 11:07:11 +0200

    refactor getting class images

--- src/app/collection/collection-home/collection-element/collection-element.component.ts
+++ src/app/collection/collection-home/collection-element/collection-element.component.ts
@@ -18,8 +18,32 @@ export class CollectionElementComponent {
   @Input() element: elementInterface = {
     "id": 0,
     "name": "Adam Nobody",
-    "class": "barbarian",
-    "race": "human",
+    "class": "Barbarian",
+    "race": "Human",
     "level": 1
   };
+
+  classImages: any[] = [
+    {class: "Barbarian", path:"assets/barbarian_icon.jpeg"},
+    {class: "Bard", path:"assets/bard_icon.jpeg"},
+    {class: "Cleric", path:"assets/cleric_icon.jpeg"},
+    {class: "Druid", path:"assets/druid_icon.jpeg"},
+    {class: "Fighter", path:"assets/fighter_icon.jpeg"},
+    {class: "Monk", path:"assets/monk_icon.jpeg"},
+    {class: "Paladin", path:"assets/paladin_icon.jpeg"},
+    {class: "Ranger", path:"assets/ranger_icon.jpeg"},
+    {class: "Rogue", path:"assets/rogue_icon.jpeg"},
+    {class: "Sorcerer", path:"assets/sorcerer_icon.jpeg"},
+    {class: "Warlock", path:"assets/warlock_icon.jpeg"},
+    {class: "Wizard", path:"assets/wizard_icon.jpeg"}
+  ];
+
+  getClassImage() {
+    let characterClass: string = this.element.class;
+    for (let image of this.classImages) {
+      if(image.class === characterClass) {
+        return image.path;
+      }
+    }
+  }
 }
